d4f82975a6f27793ff97c60e71f5a26af177b73e
Shyam-Prag <pragshyam11@gmail.com>
2025-03-13 21:43:40 +0000

    adding image split

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -31,3 +31,50 @@ def extract_markdown_links(text):
     image_regex_pattern = "(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
     matches = re.findall(image_regex_pattern, text)
     return matches 
+
+
+def split_nodes_image(old_nodes):
+    result = []
+    
+    for old_node in old_nodes:
+        # Skip non-text nodes
+        if old_node.text_type != TextType.TEXT:
+            result.append(old_node)
+            continue
+        
+        # Get the text and check for images
+        text = old_node.text
+        images = extract_markdown_images(text)
+        
+        if not images:
+            # No images found, keep node as is
+            result.append(old_node)
+            continue
+        
+        # Start with the full text
+        remaining_text = text
+        
+        # Process each image found
+        for image_alt, image_url in images:
+            #split on the first occurange of this image 
+            image_markdown = f"![{image_alt}]({image_url})"
+            parts = remaining_text.split(image_markdown, 1)
+
+            #add text before the image if not empty 
+            if parts[0]:
+                result.append(TextNode(parts[0], TextType.TEXT))
+
+            # Add the image node
+            result.append(TextNode(image_alt, TextType.IMAGE, image_url))
+
+            #update the remaining text 
+            if len(parts) > 1:
+                remaining_text = parts[1]
+            else:
+                remaining_text = ""
+        
+        #add any other remaining text
+        if remaining_text:
+            result.append(TextNode(remaining_text, TextType.TEXT))
+    
+    return result 
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,7 @@
 import unittest
 from inline_markdown import split_nodes_delimiter
 from textnode import TextNode, TextType
-from inline_markdown import extract_markdown_images, extract_markdown_links
+from inline_markdown import extract_markdown_images, extract_markdown_links, split_nodes_image#, split_nodes_link
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
@@ -97,6 +97,26 @@ class TestRegex(unittest.TestCase):
             "This is text with a [link](https://i.imgur.com/zjjcJKZ.png)"
         )
         self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+class TestInlineSplit(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 
 
 if __name__ == "__main__":
